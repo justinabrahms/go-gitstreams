415cf4d2cd054e486567b3d973f9993ec3c2c729
Justin Abrahms <justin@abrah.ms>
2013-05-05 08:56:46 -0700

    fixing test w/ updated path to method.

--- issue_comment_test.go
+++ issue_comment_test.go
@@ -23,7 +23,7 @@ func TestBodyShort(t *testing.T) {
 	body := "short one"
 	icp, err := build_icp(body)
 	if err != nil { t.Errorf("%s", err) }
-	assertEqual(t, icp.ShortBody(), body)
+	assertEqual(t, icp.Payload.Comment.ShortBody(), body)
 }
 
 
@@ -31,10 +31,10 @@ func TestBodyLong(t *testing.T) {
 	body := "longer than 80 characater string which should be split before the complete end of the string else the test will fail. Really."
 	icp, err := build_icp(body)
 	if err != nil { t.Errorf("%s", err) }
-	if len(icp.ShortBody()) > 80 {
+	if len(icp.Payload.Comment.ShortBody()) > 80 {
 		t.Errorf("Didn't truncate it to 80 characters")
 	}
-	if !strings.HasSuffix(icp.ShortBody(), "...") {
+	if !strings.HasSuffix(icp.Payload.Comment.ShortBody(), "...") {
 		t.Errorf("Didn't add elipsis at the end.")
 	}
 }
